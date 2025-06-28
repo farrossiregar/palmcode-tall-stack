e1cb6ec99b22046e1896687521004b75f0134b8a
farros shier <farros@tribunnews.com>
2025-06-28 23:40:10 +0700

    update deploy ci/cd

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,17 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    // server: {
-    //     host: '0.0.0.0',
-    //     port: 5173,
-    //     proxy: {
-    //     '/api': {
-    //         target: 'http://localhost',
-    //         changeOrigin: true,
-    //         secure: false,
-    //     },
-    //     },
-    // },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        proxy: {
+        '/api': {
+            target: 'http://localhost',
+            changeOrigin: true,
+            secure: false,
+        },
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
