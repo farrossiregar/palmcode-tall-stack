643b44e6d09de2bb1b13b3f28c5c51c549cff5ea
farros shier <farros@tribunnews.com>
2025-06-28 22:57:45 +0700

    update deploy ci/cd

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,17 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/test', fn() => 'OK');
 
 Route::apiResource('post', PostController::class);
+
+Route::get('/post', [PostController::class, 'index']);
+Route::post('/post', [PostController::class, 'store']);
+Route::get('/post/{id}', [PostController::class, 'show']);
+Route::put('/post/{id}', [PostController::class, 'update']);
+Route::delete('/post/{id}', [PostController::class, 'destroy']);
+
